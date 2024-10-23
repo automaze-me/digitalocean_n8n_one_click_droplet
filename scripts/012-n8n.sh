27bab2e8f2e1b3c49274f1df90a86b040a239c60
Joern Bungartz <joern@automaze.me>
2024-10-23 23:16:21 +0000

    Initial version of installation and setup scripts.

---
+++ scripts/012-n8n.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /opt
+git clone https://github.com/n8n-io/n8n-docker-caddy.git
+cd n8n-docker-caddy
+
+# set data directory in .env file
+sed -i "s/<directory-path>/opt/g" ".env"
+
+docker volume create caddy_data
+docker volume create n8n_data
+
+ufw allow 80
+ufw allow 443
+
+
